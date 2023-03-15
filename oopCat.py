c2810c0e0226be24e2d8354f6a0d2e0af5303f13
Alex Davidson <alex.leigh.davidson@gmail.com>
2023-03-15 21:09:46 +0000

    update to object orientation notes

--- oopCat.py
+++ oopCat.py
@@ -2,15 +2,25 @@ from oopPet import Pet
 
 
 class Cat(Pet):
+    # class variable
+    # accessible directly through the class name
+    number_of_cats = 0
+
     # __init__is very special, python reserved function
     # the constructor method
-    def __init__(self, cat_name, cat_colour):
+    def __init__(self, cat_name: str, cat_colour: str):  # type hinting
         # instance variable, aka attribute called _name
         # single underscore before name means it is private for the class
         # self._name = cat_name
         # self._colour = cat_colour
         # inheriting from superclass
         super().__init__(cat_name, cat_colour)
+        # using the class variable
+        Cat.number_of_cats += 1
+
+# type hinting is not enforced, because pythong is interpreted, not compiled language
+# in other languages which are compiled, it checks whether the variable types match what has been asked for
+    # and will not even run the code
 
     # method
     def purr(self):
@@ -26,3 +36,6 @@ class Cat(Pet):
     # setter method
     def set_height(self, cat_height):
         self._height = cat_height
+
+# the methods within the Class can contain anything that we have done previously
+# ifs, loops, inputs, returns, etc
--- oopPet.py
+++ oopPet.py
@@ -1,10 +1,12 @@
 class Pet:
 
-    def __init__(self, pet_name, pet_colour):
+    def __init__(self, pet_name, pet_colour):  # constructor
         # this full list of attributes can be applied to subclasses
         # by including super().__init__(parameters) in the def __init__ for that class
+        # can pass complex values (lists, dictionaries, other objects) not just primitive
         self._name = pet_name
         self._colour = pet_colour
+        # best practice to declare all attributes in the constructor, even if you don't have the value initially
 
     # this method doesn't need to be re-defined in the subclasses
     def make_sound(self):
